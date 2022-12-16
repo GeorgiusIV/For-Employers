955ad5cf70876d27a7e47b018b9775f9543e6736
George <TheKillaCake@gmail.com>
2022-12-16 15:57:39 +0000

    Update KruskalForMST.py

--- Kruskal for MST/KruskalForMST.py
+++ Kruskal for MST/KruskalForMST.py
@@ -1,232 +1,302 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 
-class DisjointSet():
+class DisjointSet:
+
     _id = 0
-    
-    def __init__(self, face = None):
-        
+
+    def __init__(self, face=None):
+
         # a disjoint set begins as its own parent
+
         self.parent = self
-        
+
         # if no face e.g. 'a' or 'x' is chosen, give the names 'x1','x2' ...
+
         self._id += 1
-        if face: self.face = face
-        else: self.face = "x" + str(self._id)
-        
-    def Find(self):
-        
+        if face:
+            self.face = face
+        else:
+            self.face = 'x' + str(self._id)
+
+    def find(self):
+
         # if this disjoint set is its own parent, return it
+
         if self.parent == self:
             return self
-        
-        # if not, then perform this same check on its parent, this will recurse until a parent is found
         else:
-            return self.parent.Find()
 
-    def Union(self,newParent):
-        
+        # if not, recurse on the parent, repeat until parent is found
+
+            return self.parent.find()
+
+    def union(self, newParent):
+
         # change the parent of this disjoint set to newParent
-        self.parent = newParent
-    
 
-class Graph():
-    
+        self.parent = newParent
+
+
+class Graph:
+
     def __init__(self, V):
-        
+
         self.verts = V
         self.edges = list()
-    
-    
-    def UnionFind(self):
-        
+
+    def union_find(self):
+
         # create a Disjoint Set for each vertex of this graph
+
         disjoints = [DisjointSet(v) for v in self.verts]
-        
-        # iterate through each edge of this graph, collect the verts on either end
+
+        # iterate through each edge of this graph, collect the verts either side
+
         for e in self.edges:
-            u,v = e[0],e[1]
-            
+            (u, v) = (e[0], e[1])
+
             # find the location of each vertex in self.verts
+
             u_index = self.verts.index(u)
             v_index = self.verts.index(v)
-            
+
             # overwrite the verts with their corresponding disjoint set
+
             u = disjoints[u_index]
             v = disjoints[v_index]
 
             # if the disjoint sets share a parent, then a cycle is found
-            if u.parent.Find() == v.parent.Find():
+
+            if u.parent.Find() == v.parent.find():
                 return True
-            
-            # if they do not share a parent, Union them, now they do share a parent, if they are ever called upon again
             else:
-                u.Union(v)
-                
-        # if all of the edges are iterated, and no cycles are found, return false
+
+            # if they do not share a parent, Union them, now they do.
+
+                u.union(v)
+
+        # if all edges are iterated, and no cycles are found, return false
+        
         return False
-        
-    
-    
-    def Kruskal(self):
-        
+
+    def kruskal(self):
+
         # produce a clone of this graph, with the same vertices, and no edges
+
         MST = Graph(self.verts)
-        
+
         # organize the edges of this graph by weight ascending
-        E = self.reorderEdges()
-        
+
+        E = self.reorder_edges()
+
         # iterate through each edge, and add the edge to the clone
+
         for e in E:
             MST += e
-            
+
             # if adding the edge produces a cycle, them remove the edge
-            if MST.UnionFind():
+
+            if MST.union_find():
                 MST -= e
 
-        # once all edges are removed, return the clone, it is the Minimum Spanning Tree of this graph
+        # once reorganized, return the clone - the Minimum Spanning Tree
+
         return MST
-    
-            
-        
-        
-        
-            
+
     def __add__(self, operand):
-        
-        # if the operand is a list, iterate through each entry of that list 
+
+        # if operand is list, iterate through each entry of the list
+
         if type(operand) == type(list()):
             for o in operand:
                 self.__add__(o)
-        
-        # if the operand b is a string, treat it as a vertex, and add it directly to self.verts
         elif type(operand) == type(str()):
+
+        # if operand is string, treat it as vertex - add directly to self.verts
+
             self.verts += [operand]
-            
-        # if the operand b is a tuple, treat it as an edge and add it directly to self.edges
         elif type(operand) == type(tuple()):
-            self.edges += [(operand[0],operand[1],operand[2])]
-            
-        # this is here just to complete all cases for the elif statement
+
+        # if operand is tuple, treat it as edge - add directly to self.edges
+
+            self.edges += [(operand[0], operand[1], operand[2])]
         else:
+
+        # complete all cases for the if-elif-else statement
+
             pass
-        
+
         return self
-        
-        
+
     def __sub__(self, operand):
-        
-        # if the operand is a list, iterate through each entry of that list 
+
+        # if operand is list, iterate through each entry of the list
+
         if type(operand) == type(list()):
             for o in operand:
                 self.__add__(o)
-        
-        # if the operand is a string, treat it as a vertex, and remove it directly from self.verts
         elif type(operand) == type(str()):
+
+        # if operand is string, treat it as vertex - remove from self.verts
+
             self.verts -= [operand]
-            
-        # if the operand is a tuple, treat it as an edge and remove it directly from self.edges
         elif type(operand) == type(tuple()):
-            self.edges.remove((operand[0],operand[1],operand[2]))
-            
-        # this is here just to complete all cases for the elif statement
-        else: 
+
+        # if operand is tuple, treat it as edge - remove from self.edges
+
+            self.edges.remove((operand[0], operand[1], operand[2]))
+        else:
+
+        # complete all cases for the if-elif-else statement
+
             pass
-        
+
         return self
-            
-            
-    def reorderEdges(self):
-        
+
+    def reorder_edges(self):
+
         # create a list of the weights of this graph
+
         W = [e[2] for e in self.edges]
-        
+
         # sort the weights in ascending order
-        W = self.Mergesort(W)
-        
+
+        W = self.merge_sort(W)
+
         # create a list to store the reordered edges
+
         E = list()
-        
-        # for each edge in this graph, find the corresponding weight from the reorganized weights
+
+        # per edge in the graph, find the shared weight from reorganized weights
+
         for w in W:
             for e in self.edges:
-                
-                # match the position of the edge e, with the position of its corresponding weight
+
+                # match edge e, with the position of its shared weight
+
                 if e not in E and e[2] == w:
                     E += [e]
                     break
-                    
+
         # return the reorganized edges
+
         return E
-            
-         
-    def Mergesort(self, lst):
-    
+
+    def merge_sort(self, lst):
+
         # create pointers at the start and end of the list
+
         start = 0
         end = len(lst)
-    
+
         # if the list can still be split, split it
+
         if end > 1:
-            
+
             # create a pointer to the middle, around which to split the list
+
             mid = end // 2
-        
+
             # split the list into left and right sides
-            L = self.Mergesort(lst[start:mid])
-            R = self.Mergesort(lst[mid:end+1])
-        
-            # create pointers for iterating through the list, collect the first elements, and produce a return list
-            i,j = 0,0
-            l = L[0] 
-            r = R[0] 
-            newlst = []
-        
-            # begin merging the two lists
-            #while either list L or R, still has an element inside
-            while i < len(L) or j < len(R):
+
+            L = self.merge_sort(lst[start:mid])
+            R = self.merge_sort(lst[mid:end + 1])
+
+            # create pointers for iterating through the list
+
+            (i, j) = (0, 0)
             
-                # if list L still has an element, and that element is smaller than list R's element
+            #collect the first elements, and produce a return list
+            
+            l = L[0]
+            r = R[0]
+            newlst = []
+
+            # begin merging the two lists
+            # while either list L or R, still has an element inside
+
+            while i < len(L) or j < len(R):
+
+                # if list L still has an element, 
+                # and that element is smaller than list R's element
+
                 if l < r and i < len(L):
-                    
-                    # add the smaller element to the return list, increment the pointers position in the list
+
+                    # add the smaller element to the return list, 
+                    # increment the pointers position in the list
+
                     newlst += [l]
                     i += 1
-                    
-                    # if there is an element at the pointer's new position, add it. If not, treat the element as infinity
-                    try: l = L[i] 
-                    except IndexError: l = float('inf')
-                        
-                # a repeat of what happens to L, for R.
+
+                    # if there is an element at the pointer's new position,
+                    # add it. If not, treat the element as infinity
+
+                    try:
+                        l = L[i]
+                    except IndexError:
+                        l = float('inf')
                 elif j < len(R):
+
+                # a repeat of what happens to L, for R.
+
                     newlst += [r]
                     j += 1
-                    try: r = R[j] 
-                    except IndexError: r = float('inf')
-                        
-                # this is here solely to complete all cases of the elif statement
+                    try:
+                        r = R[j]
+                    except IndexError:
+                        r = float('inf')
                 else:
-                    pass
-                    
-            # finally, return the newly ordered list
-            return newlst
-        
-        # if the list cannot be split, but contains an element, then return the single element and begin merging
-        elif end == 1:
-            return lst
-        
-        # if the list does not contain an element, return an infinite value that will never be appended
-        else:
-            return [float('inf')]
-        
-        
-        
-def main():
-    verts = ["a","b","c","d","e","f","g","h","i"]
-    G = Graph(verts)
-    G += [("a","b",3),("a","d",12),("b","c",7),("c","e",8),("d","e",10),("e","f",9),("e","h",12),("f","g",10),("h","i",3)]
 
-    print(G.Kruskal().edges)
- 
-if __name__ == "__main__":
+                # complete all cases of the if-elif-else statement
+
+                    pass
+
+            # finally, return the newly ordered list
+
+            return newlst
+        elif end == 1:
+
+        # if the list cannot be split, but contains an element, 
+        # then return the single element and begin merging
+
+            return lst
+        else:
+
+        # if the list does not contain an element,
+        # return an infinite value that will never be appended
+
+            return [float('inf')]
+
+
+def main():
+    verts = [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        ]
+    G = Graph(verts)
+    G += [
+        ('a', 'b', 3),
+        ('a', 'd', 12),
+        ('b', 'c', 7),
+        ('c', 'e', 8),
+        ('d', 'e', 10),
+        ('e', 'f', 9),
+        ('e', 'h', 12),
+        ('f', 'g', 10),
+        ('h', 'i', 3),
+        ]
+
+    print G.kruskal().edges
+
+
+if __name__ == '__main__':
     main()
-    
